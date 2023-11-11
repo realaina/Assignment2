3e39eba29bf1154799d6d13df3caf2dc9fbba99d
realaina <116939983+realaina@users.noreply.github.com>
2023-11-11 14:59:37 +0600

    Add files via upload

---
+++ task1.dart
@@ -0,0 +1,13 @@
+/////task 1 Write a Dart function to show the date and time at now.
+
+void showCurrentDateTime() {
+  DateTime now = DateTime.now();
+  print('Current Date and Time: $now');
+}
+
+void main() {
+  showCurrentDateTime();
+}
+  
+
+
